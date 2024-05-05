ffb4a8408d67ca8dfb989ddb395c8dc409e8c53f
GUAN MING <guanmingchiu@gmail.com>
2024-05-05 20:07:23 +0800

    refactor: update store layout

--- src/app/store/[storeId]/page.tsx
+++ src/app/store/[storeId]/page.tsx
@@ -4,64 +4,54 @@ import { ChevronLeft } from "lucide-react";
 import { BellPlus } from "lucide-react";
 
 import { Separator } from "@/components/ui/separator";
-
-import Product from "./_component/Product";
+import Dish from "./_component/Dish";
 
 const shop = ["Food Store"];
 const products = [
   {
     id: "1",
-    image:
+    images: [
       "https://images.chinatimes.com/newsphoto/2023-06-13/1024/20230613002377.jpg",
-    image1: "https://letsplay.tw/wp-content/uploads/20190630221316_45.jpg",
-    image2:
+      "https://letsplay.tw/wp-content/uploads/20190630221316_45.jpg",
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8EBFM8hvxZZCu8gc0k7Cm-gDVokueuBa-41Hpqzz3fQ&s",
-    productName: "Pizza",
-    cost: "1000",
-    remainAmount: 2,
-    comment:
+    ],
+
+    name: "Pizza",
+    price: 1000,
+    quantity: 2,
+    description:
       "緊急通知，我們還有少量便當剩餘，現在下單立享折扣 ! 這些美味的便當是用新鮮食材製作的...",
   },
   {
     id: "2",
-    image: "https://letsplay.tw/wp-content/uploads/20190630221316_45.jpg",
-    image1:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8EBFM8hvxZZCu8gc0k7Cm-gDVokueuBa-41Hpqzz3fQ&s",
-    image2:
+    images: [
       "https://images.chinatimes.com/newsphoto/2023-06-13/1024/20230613002377.jpg",
-    productName: "Icecream",
-    cost: "1000",
-    remainAmount: 2,
-    comment:
-      "緊急通知，我們還有少量便當剩餘，現在下單立享折扣 ! 這些美味的便當是用新鮮食材製作的...",
+      "https://letsplay.tw/wp-content/uploads/20190630221316_45.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8EBFM8hvxZZCu8gc0k7Cm-gDVokueuBa-41Hpqzz3fQ&s",
+    ],
+
+    name: "Pizza",
+    price: 1000,
+    quantity: 2,
+    description:
+      "緊急通知，我們還有少量便當剩餘，現在下單立享折扣 ! 這些美味的便當是用新鮮食材製作的...緊急通知，我們還有少量便當剩餘，現在下單立享折扣 ! 這些美味的便當是用新鮮食材製作的...緊急通知，我們還有少量便當剩餘，現在下單立享折扣 ! 這些美味的便當是用新鮮食材製作的...緊急通知，我們還有少量便當剩餘，現在下單立享折扣 ! 這些美味的便當是用新鮮食材製作的...緊急通知，我們還有少量便當剩餘，現在下單立享折扣 ! 這些美味的便當是用新鮮食材製作的...",
   },
 ];
 
-export default function Sell() {
+export default function Store() {
   return (
     <>
-      <div className="flex w-full items-center gap-4">
-        <ChevronLeft className="h-10 w-10 cursor-pointer p-2 hover:rounded-full hover:bg-gray-100/50" />
-        <p className="grow text-center text-lg font-bold md:text-2xl lg:text-4xl">
-          {shop}
-        </p>
-        <BellPlus className="h-10 w-10 cursor-pointer p-2 hover:rounded-full hover:bg-gray-100/50" />
+      <div className="flex w-full items-center justify-between text-center">
+        <ChevronLeft className="h-4 w-4 cursor-pointer" />
+        <h1 className="text-center text-xl font-semibold">{shop}</h1>
+        <BellPlus className="h-4 w-4 cursor-pointer" />
       </div>
 
       <Separator />
 
-      <div className="mb-4 flex flex-col gap-4 md:gap-8">
+      <div className="flex flex-col max-md:space-y-4 md:grid md:grid-cols-2">
         {products.map((product) => (
-          <Product
-            key={product.id}
-            image={product.image}
-            image1={product.image1}
-            image2={product.image2}
-            productName={product.productName}
-            cost={product.cost}
-            remainAmount={product.remainAmount}
-            comment={product.comment}
-          />
+          <Dish key={product.id} dish={product} />
         ))}
       </div>
     </>
