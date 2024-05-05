f18b175ddfc9a65e9e04d23562df86f72452b8ae
huangfishburger <sophia.ythuang@gmail.com>
2024-05-05 22:07:36 +0800

    feat: post-card

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -4,15 +4,14 @@ import { Card } from "@/components/ui/card";
 
 import Image from "next/image";
 
-type PurchaseCard = {
-    id: number;
-    storeName: string;
-    price: number;
-    state: string;
+type PostCard = {
+    title: string;
+    context: string;
+    remaining: number;
     photo: string;
   };
 
-export function PurchaseCard({ id, storeName, price, state, photo }: PurchaseCard) {
+export function PostCard({ title, context, remaining, photo }: PostCard) {
     return (
         <Card className="m-2 lg:m-4 p-2 lg:p-4">
             <div className="flex gap-x-4">
@@ -20,16 +19,16 @@ export function PurchaseCard({ id, storeName, price, state, photo }: PurchaseCar
                     src={`/${photo}`}
                     width={96}
                     height={96}
-                    alt={storeName}
+                    alt={title}
                 />
                 <div>
                     <div className="flex items-center gap-x-8">
-                        <p className="text-lg lg:text-2xl font-bold">{storeName}</p>
-                        <p className="text-md lg:text-lg">{price}</p>
+                        <p className="text-lg lg:text-2xl font-bold">{title}</p>
+                        <p className="text-md lg:text-lg text-green-500">剩餘: {remaining}</p>
                     </div>
-                    <div className="text-md lg:text-lg font-normal text-muted-foreground pt-4">
-                        {state}
-                    </div>
+                    <p className="text-md lg:text-lg font-normal text-muted-foreground pt-2 lg:pt-4">
+                        {context.slice(0, 30)}...
+                    </p>
                 </div>
             </div>
         </Card>
