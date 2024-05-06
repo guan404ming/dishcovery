fae504273e111703e071ec4aea3246a22faa40e3
Yu-Ting Chen <105917993+yuting1008@users.noreply.github.com>
2024-05-06 17:02:36 +0800

    feat: add schema (#41)
    Co-authored-by: GUAN MING <guanmingchiu@gmail.com>

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,18 +1,176 @@
-import { index, pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import {
+  index,
+  integer,
+  pgTable,
+  pgEnum,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
+export const statusEnum = pgEnum("status", [
+  "waiting",
+  "confirmed",
+  "finished",
+  "cancelled",
+]);
+export const categoryEnum = pgEnum("category", [
+  "taiwanese",
+  "japanese",
+  "american",
+  "healthy meal",
+  "pastry",
+  "fruit",
+]);
+
+// user
 export const userTable = pgTable(
   "users",
   {
     id: serial("id").primaryKey(),
-    username: varchar("name", { length: 100 }).notNull(),
-    email: varchar("email", { length: 100 }).notNull().unique(),
+    email: varchar("email", { length: 50 }).notNull().unique(),
+    name: varchar("name", { length: 50 }).notNull(),
+    role: varchar("role", { enum: ["Admin", "User"] }).notNull(),
   },
   (table) => ({
     emailIndex: index("email_index").on(table.email),
   }),
 );
 
+export const cartTable = pgTable("carts", {
+  id: serial("id").primaryKey(),
+  userId: serial("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+  createTime: timestamp("create_time").defaultNow().notNull(),
+  dishId: serial("dish_id")
+    .notNull()
+    .references(() => dishTable.id, { onDelete: "cascade" }),
+  quantity: integer("quantity").notNull().default(1),
+  storeId: serial("store_id")
+    .notNull()
+    .references(() => storeTable.id, { onDelete: "cascade" }),
+});
+
+export const commentTable = pgTable(
+  "comments",
+  {
+    id: serial("id").primaryKey(),
+    content: varchar("comment_content", { length: 200 }).notNull(),
+    createTime: timestamp("create_time").defaultNow().notNull(),
+    lastUpdateTime: timestamp("last_update_time").defaultNow().notNull(),
+    userId: serial("user_id")
+      .notNull()
+      .references(() => userTable.id, { onDelete: "cascade" }),
+    postId: serial("post_id")
+      .notNull()
+      .references(() => postTable.id, { onDelete: "cascade" }),
+  },
+  (table) => ({
+    userIdIndex: index("user_id_index").on(table.userId),
+    postIdIndex: index("post_id_index").on(table.postId),
+  }),
+);
+
 export const bannerTable = pgTable("banners", {
   id: serial("id").primaryKey(),
+  userId: serial("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
   url: varchar("url").notNull(),
+  createTime: timestamp("create_time").defaultNow().notNull(),
+});
+
+export const categoryCollectionTable = pgTable("post_collections", {
+  userId: serial("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+  category: categoryEnum("category").notNull(),
+});
+
+// store
+export const storeTable = pgTable("stores", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 50 }).notNull(),
+  address: varchar("address", { length: 50 }).notNull(),
+  phone: varchar("phone", { length: 50 }).notNull(),
+  userId: serial("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+});
+
+export const dishTable = pgTable("dishes", {
+  id: serial("id").primaryKey(),
+  quantity: integer("quantity").notNull().default(1),
+  category: categoryEnum("category").notNull(),
+  storeId: serial("store_id")
+    .notNull()
+    .references(() => storeTable.id, { onDelete: "cascade" }),
+  name: varchar("name", { length: 100 }).notNull(),
+  price: integer("price").notNull(),
+  description: varchar("description", { length: 100 }).notNull(),
+});
+
+export const storeReservationTable = pgTable("store_reservations", {
+  id: serial("id").primaryKey(),
+  userId: serial("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+  createTime: timestamp("create_time").defaultNow().notNull(),
+  storeId: serial("store_id")
+    .notNull()
+    .references(() => storeTable.id, { onDelete: "cascade" }),
+  dishId: serial("dish_id")
+    .notNull()
+    .references(() => dishTable.id, { onDelete: "cascade" }),
+  quantity: integer("quantity").notNull().default(1),
+  status: statusEnum("status").notNull().default("waiting"),
+});
+
+export const storeCollectionTable = pgTable("store_collections", {
+  userId: serial("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+  storeId: serial("store_id")
+    .notNull()
+    .references(() => storeTable.id, { onDelete: "cascade" }),
+});
+
+// post
+export const postTable = pgTable("posts", {
+  id: serial("id").primaryKey(),
+  title: varchar("title", { length: 50 }).notNull(),
+  description: varchar("description", { length: 100 }).notNull(),
+  createTime: timestamp("create_time").defaultNow().notNull(),
+  lastUpdateTime: timestamp("last_update_time").defaultNow().notNull(),
+  location: varchar("location", { length: 50 }).notNull(),
+  userId: serial("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+});
+
+export const postDishTable = pgTable("post_dishes", {
+  id: serial("id").primaryKey(),
+  postId: serial("post_id")
+    .notNull()
+    .references(() => postTable.id, { onDelete: "cascade" }),
+  quantity: integer("quantity").notNull().default(1),
+  category: categoryEnum("category").notNull(),
+  dishName: varchar("dishName", { length: 100 }).notNull(),
+});
+
+export const postReservationTable = pgTable("post_reservations", {
+  id: serial("id").primaryKey(),
+  userId: serial("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+  createTime: timestamp("create_time").defaultNow().notNull(),
+  postId: serial("post_id")
+    .notNull()
+    .references(() => postTable.id, { onDelete: "cascade" }),
+  dishId: serial("dish_id")
+    .notNull()
+    .references(() => dishTable.id, { onDelete: "cascade" }),
+  quantity: integer("quantity").notNull().default(1),
+  status: statusEnum("status").notNull().default("waiting"),
 });
--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,5 +1,38 @@
-import type { userTable, bannerTable } from "@/db/schema";
+import type { 
+    userTable, 
+    bannerTable, 
+    cartTable, 
+    commentTable, 
+    categoryCollectionTable, 
+    storeTable, 
+    dishTable,
+    storeReservationTable,
+    storeCollectionTable,
+    postTable,
+    postDishTable,
+    postReservationTable
+} from "@/db/schema";
 
 export type SelectUser = typeof userTable.$inferSelect;
 
+export type SelectCart = typeof cartTable.$inferSelect;
+
+export type SelectComment = typeof commentTable.$inferSelect;
+
 export type SelectBanner = typeof bannerTable.$inferSelect;
+
+export type SelectCategoryCollection = typeof categoryCollectionTable.$inferSelect;
+
+export type SelectStore = typeof storeTable.$inferSelect;
+
+export type SelectDish = typeof dishTable.$inferSelect;
+
+export type SelectStoreReservation = typeof storeReservationTable.$inferSelect;
+
+export type SelectStoreCollection = typeof storeCollectionTable.$inferSelect;
+
+export type SelectPost = typeof postTable.$inferSelect;
+
+export type SelectPostDish = typeof postDishTable.$inferSelect;
+
+export type SelectPostReservation = typeof postReservationTable.$inferSelect;
