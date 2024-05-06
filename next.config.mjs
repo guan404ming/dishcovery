9cd932390f996d3fadf45fc60a8daf98780de3af
GUAN MING <guanmingchiu@gmail.com>
2024-05-06 21:17:43 +0800

    env: init oss

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["shoplineimg.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "of3cnlsryfyioeqq.public.blob.vercel-storage.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "shoplineimg.com",
+        port: "",
+      },
+    ],
   },
 };
 
