01bdced2e0547ece949fa9587c1ab725a758da0e
GUAN MING <guanmingchiu@gmail.com>
2024-05-06 22:27:20 +0800

    feat: work with db

--- src/app/api/banners/route.ts
+++ src/app/api/banners/route.ts
@@ -2,16 +2,29 @@ import { NextResponse } from "next/server";
 
 import { uploadFile } from "../utils";
 
+import { db } from "@/db";
+import { bannerTable } from "@/db/schema";
+
 export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const filename = searchParams.get("filename");
 
   if (filename == null)
-    return NextResponse.json({
-      status: "400",
-      message: "Invalid request",
-    });
+    return NextResponse.json({ error: "Invalid request" }, { status: 400 });
   const blob = await uploadFile(`banner/${filename}`, await request.blob());
 
-  return NextResponse.json(blob);
+  try {
+    const [banner] = await db
+      .insert(bannerTable)
+      .values({
+        userId: 1,
+        url: blob.url,
+      })
+      .returning()
+      .execute();
+    return NextResponse.json(banner);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Failed to create banner" }, { status: 500 });
+  }
 }
