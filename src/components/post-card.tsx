4f7b2920eea5f4ef7bdb7009f1554050f9e2aa14
GUAN MING <guanmingchiu@gmail.com>
2024-05-06 15:49:56 +0800

    style: unify post-card

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,41 +1,82 @@
 "use client";
 
+import { useState } from "react";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import { PlusCircle, MinusCircle } from "lucide-react";
 
-import { Card } from "@/components/ui/card";
+import { Card } from "./ui/card";
 
-type PostCard = {
+export interface Post {
   title: string;
-  context: string;
-  remaining: number;
-  photo: string;
-};
+  content: string;
+  picUrl: string;
+}
 
-export function PostCard({ title, context, remaining, photo }: PostCard) {
+export function PostCard({
+  post,
+  isCounter,
+}: {
+  post: Post;
+  isCounter?: boolean;
+}) {
+  const router = useRouter();
+  const [amount, setAmount] = useState(0);
+
+  const updateAmount = (type: string) => {
+    if (type == "minus") {
+      if (amount > 0) {
+        setAmount(amount - 1);
+      }
+    } else [setAmount(amount + 1)];
+  };
+
   return (
-    <Card className="m-2 p-2 lg:m-4 lg:p-4">
-      <div className="flex justify-between gap-x-4">
-        <Image src={`/${photo}`} width={96} height={96} alt={title} />
-        <div className="flex-grow">
-          <div className="flex items-center gap-x-8">
-            <p className="text-lg font-bold lg:text-2xl">{title}</p>
+    <Card
+      key={post.title}
+      className="z-0 flex h-fit max-h-24 w-full cursor-pointer flex-row text-ellipsis text-center md:max-h-24 lg:max-h-56"
+      onClick={() => {
+        router.push("post/123");
+      }}
+    >
+      <Image
+        src={"/1.jpeg"}
+        alt={`${post.title}`}
+        className="aspect-[1/1] object-cover"
+        width={100}
+        height={100}
+      />
+      <div className="flex flex-col items-start truncate px-4 py-3 text-left text-xs font-semibold text-foreground">
+        <div className="mb-1 flex flex-col space-y-0.5">
+          <div>{post.title}</div>
+          <div className="text-xs font-light text-muted-foreground">
+            Remaining: 100{" "}
           </div>
-          <p className="text-md pt-2 font-normal text-muted-foreground lg:pt-4 lg:text-lg">
-            {context.slice(0, 20)}...
-          </p>
         </div>
-        <div className="flex flex-row items-center gap-x-2">
-          <p className="text-xs lg:text-lg">
-            <MinusCircle />
-          </p>
-          <p className="text-xs lg:text-lg">{remaining}</p>
-          <p className="text-xs lg:text-lg">
-            <PlusCircle />
-          </p>
-        </div>
+        <span className="block truncate text-wrap text-left font-normal text-muted-foreground">
+          {post.content}
+        </span>
       </div>
+      
+      {isCounter && (
+        <div className="mx-2 flex items-center space-x-1">
+          <MinusCircle
+            className="h-4 w-4 cursor-pointer"
+            onClick={() => {
+              updateAmount("minus");
+            }}
+          />
+          <p>{amount}</p>
+          <PlusCircle
+            className="h-4 w-4 cursor-pointer"
+            onClick={() => {
+              updateAmount("add");
+            }}
+          />
+        </div>
+      )}
     </Card>
   );
 }
