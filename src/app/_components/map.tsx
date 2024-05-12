933dcfefcd9c3abccf09133bc36d798f6352c847
Benson Chiu 邱秉辰 <b10705047@g.ntu.edu.tw>
2024-05-12 21:03:32 +0800

    feat(app/_components/map.tsx): add real google map with a predefined marker and info box
    feat #36

--- src/app/_components/map.tsx
+++ src/app/_components/map.tsx
@@ -1,17 +1,32 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
+import {
+  APIProvider,
+  Map, 
+  AdvancedMarker,
+  Pin, 
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
 
-export default function Map() {
+export default function MapView() {
+  const position = { lat: 25.0129, lng: 121.5371};
+  const [open, setOpen] = useState(false);
   return (
-    <div className="flex justify-center">
-      <Image
-        src="/map.png"
-        alt="Map"
-        width={500}
-        height={500}
-        className="aspect-square w-full rounded-md object-cover md:aspect-[6/1]"
-      />
-    </div>
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+      <div className="h-[300px] w-[500px]">
+        <Map zoom={15} center={position} mapId={process.env.NEXT_PUBLIC_MAP_ID}>
+          <AdvancedMarker position={position} onClick={() => setOpen(true)} >
+            <Pin background={"black"} borderColor={"black"} glyphColor={"white"}/>
+          </AdvancedMarker>
+          {open && (<InfoWindow position={position} onCloseClick={() => setOpen(false)} pixelOffset={[0, -40]}>
+            <div>
+              <h1>電神邱冠銘在這裡發便當</h1>
+            </div>
+          </InfoWindow>)}
+        </Map>
+      </div>
+    </APIProvider>
   );
 }
